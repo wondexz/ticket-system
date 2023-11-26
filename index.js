aa6ff1958475576277f0006dc1264be10464fe8d
wondexz <141147771+wondexz@users.noreply.github.com>
2023-11-26 18:33:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,50 +3,10 @@ const Discord = require('discord.js');
 const config = require('./config.json');
 const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'MESSAGE_CONTENT'] });
 
-client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
-}
-
 client.on('ready', () => {
   console.log(`${client.user.tag} Hazır!`);
 });
 
-client.on('messageCreate', (message) => {
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/);
-  const commandName = args.shift().toLowerCase();
-
-  if (!client.commands.has(commandName)) return;
-
-  const command = client.commands.get(commandName);
-
-  try {
-    command.execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.reply('Komutu çalıştırırken bir hata oluştu!');
-  }
-});
-
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-
-  const { commandName } = interaction;
-
-  if (!client.commands.has(commandName)) return;
-
-  try {
-    await client.commands.get(commandName).execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({ content: 'Komutu çalıştırırken bir hata oluştu!', ephemeral: true });
-  }
-});
 // destek oluşturma
 const categoryId = config.destek_kategori_id;
 const roleId = config.destek_sorumlusu_id;
